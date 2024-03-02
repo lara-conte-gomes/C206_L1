3719071b1cfa8ad9c06f3a330908f155ebdaca76
lara-conte-gomes <laracontegomes123@gmail.com>
2024-03-02 11:41:10 -0300

    Add files via upload
    Exercício da primeira aula prática de POO, C206-L1.

---
+++ PrimeiraAula/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Minha primeira aula de POO com Java. ");
+
+        System.out.println("Minha primeira aula de POO com Java.");
+        System.out.print("Estamos usando o Netbeans!");
+    }
+}
